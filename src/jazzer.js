9628911b78c49b68a5718ea8937ea1fdb7bf0521
Robin Löffel <r.loeffel@hispeed.ch>
2017-02-15 22:46:14 +0100

    :racehorse: improve performance and refactor stuff
    - cache container node
    - use document.open, document.write, document.close
    - rename link to url for better distinguishing
    - rewrite getLinks and rename to refreshEventListeners

--- src/jazzer.js
+++ src/jazzer.js
@@ -1,21 +1,24 @@
 /*
-    links       // (string)     css selector by which the links triggering smoothLoad will be fetched
-                //              defaults to '[data-jazzer-trigger]'
+    links           // (string)     css selector by which the links triggering smoothLoad will be fetched
+                    //              defaults to '[data-jazzer-trigger]'
 
-    container   // (string)     css selector by which the container will be identified, should be uniqe
-                //              defaults to 'jazzer'
+    container       // (string)     css selector by which the container will be identified, should be uniqe
+                    //              defaults to '#jazzer'
 
-    changeClass // (string)     css class to apply during the dom change
-                //              defaults to 'jazzer-changing'
+    changeClass     // (string)     css class to apply during the dom change
+                    //              defaults to 'jazzer-changing'
 
-    duration    // (int)        duration of the dom change transition in ms
-                //              defaults to 500
+    duration        // (int)        duration of the dom change transition in ms
+                    //              defaults to 500
 
-    url         // (bool)       if the url should be changed or not
-                //              defaults to true
+    url             // (bool)       if the url should be changed or not
+                    //              defaults to true
 
-    linkList    // (NodeList)   links that trigger smoothLoad
-                //              will be populated by fetchLinks()
+    linkNodes       // (NodeList)   link elements that trigger jazzer on click
+                    //              will be populated by getLinks()
+
+    containerNode   // (node)       dom element of which the contents will be changed
+                    //              will be defined when calling jazzer()
 */
 
 let links,
@@ -23,7 +26,8 @@ let links,
     changeClass,
     duration,
     url,
-    linkList;
+    linkNodes,
+    containerNode;
 
 const jazzer = (settings = {}) => {
     links = settings.links || '[data-jazzer-trigger]';
@@ -32,28 +36,27 @@ const jazzer = (settings = {}) => {
     duration = settings.duration || 500;
     url = settings.url || true;
 
-    fetchLinks();
+    containerNode = document.querySelector(container);
+    linkNodes = document.querySelectorAll(links);
+
+    refreshEventListeners();
 };
 
-const fetchLinks = () => {
-    // skip on initial page load
-    if (linkList) {
-        for (let i = 0; i < linkList.length; i++) {
-            // remove the event listeners of the old links
-            linkList[i].removeEventListener('click', loadPage);
-        }
+const refreshEventListeners = () => {
+    // remove the listeners of the old linkNodes
+    for (let i = 0; i < linkNodes.length; i++) {
+        linkNodes[i].removeEventListener('click', loadPage);
     }
 
-    // get the new links
-    linkList = document.querySelectorAll(links);
+    linkNodes = document.querySelectorAll(links);
 
-    if (linkList.length === 0) {
+    if (linkNodes.length === 0) {
         throw new Error(`jazzer couldn't find any links matching the css selector "${links}".`);
     }
 
-    for (let i = 0; i < linkList.length; i++) {
-        // add event listeners to the new links
-        linkList[i].addEventListener('click', loadPage);
+    // set the listeners of the new linkNodes
+    for (let i = 0; i < linkNodes.length; i++) {
+        linkNodes[i].addEventListener('click', loadPage);
     }
 };
 
@@ -64,38 +67,39 @@ const loadPage = (event) => {
     loadContent(event.currentTarget.href);
 };
 
-const loadContent = (link) => {
+const loadContent = (url) => {
     let request = new XMLHttpRequest();
 
     request.addEventListener('readystatechange', () => {
         if (request.readyState === 4) {
             // call updateDom() when the request has been successfully processed, else throw an error
             if (request.status === 200) {
-                updateDom(request.responseText, link);
+                updateDom(request.responseText, url);
             } else {
-                throw new Error(`jazzer couldn't get "${link}", the server responded with "${request.status} ${request.statusText}".`);
+                throw new Error(`jazzer couldn't get "${url}", the server responded with "${request.status} ${request.statusText}".`);
             }
         }
     });
 
-    request.open('get', link, true);
+    request.open('get', url, true);
     request.send();
 };
 
-const updateDom = (newDom, link) => {
+const updateDom = (newDom, url) => {
     // fetch the container on this dom and create a new document
-    let containerNode = document.querySelector(container),
-        newDocument = document.implementation.createHTMLDocument('');
+    let newDocument = document.implementation.createHTMLDocument('');
 
     if (!containerNode) {
         throw new Error(`jazzer couldn't find any element matching the css selector "${container}".`);
     }
 
     // hide the container
-    containerNode.classList.toggle(changeClass);
+    containerNode.classList.add(changeClass);
 
     // populate the new document with the dom (string) we got via ajax
-    newDocument.body.insertAdjacentHTML('beforeend', newDom);
+    newDocument.open();
+    newDocument.write(newDom);
+    newDocument.close();
 
     // get the markup of the container inside the new document
     let newMarkup = newDocument.querySelector(container).innerHTML;
@@ -104,26 +108,26 @@ const updateDom = (newDom, link) => {
     setTimeout(() => {
         containerNode.innerHTML = newMarkup;
 
+        refreshEventListeners();
+
         let jazzerChangedEvent = document.createEvent('Event');
         jazzerChangedEvent.initEvent('jazzerChanged', true, true);
         window.dispatchEvent(jazzerChangedEvent);
-
-        fetchLinks();
     // duration + 5 so we're sure the transition is actually done before changing the content
     }, duration + 5);
 
     // set the new title and add an entry to the browser history
     if (url) {
-        document.title = newDocument.body.innerHTML.match(/<title>(.*)<\/title>/)[1];
+        document.title = newDocument.title;
 
-        history.pushState({path: link}, '', link);
+        history.pushState({path: url}, '', url);
     }
 };
 
 const showNode = () => {
     // remove changeClass as soon as a new paint cycle starts
     setTimeout(() => {
-        document.querySelector(container).classList.toggle(changeClass);
+        containerNode.classList.remove(changeClass);
     });
 };
 
