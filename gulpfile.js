2544e8ae6aeb3b75742bf735bc938923898abe25
Robin Löffel <robin.loeffel@unic.com>
2018-02-15 15:50:14 +0100

    :package: Switch to rollup-stream, further simplify

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,46 @@
 const gulp = require('gulp');
-const betterRollup = require('gulp-better-rollup');
 const rollupBabel = require('rollup-plugin-babel');
-const runSequence = require('run-sequence');
 const del = require('del');
+const rollupStream = require('rollup-stream');
+const rollup = require('rollup');
+const source = require('vinyl-source-stream');
 const pkg = require('./package.json');
 
-gulp.task('clean', () => del('dist'));
+const config = {
+    paths: {
+        index: 'src/jazzer.js',
+        dist: 'dist/'
+    },
+    babel: {
+        presets: [
+            ['@babel/preset-env', {
+               targets: {
+                   ie: 11,
+                   browsers: 'last 2 versions'
+               },
+               useBuiltIns: 'usage',
+               modules: false,
+               debug: true
+           }]
+       ],
+       ignore: ['node_modules']
+    }
+}
+
+gulp.task('clean', () => del(config.paths.dist));
 
 gulp.task('make', () => {
-    return gulp.src('src/jazzer.js')
-        .pipe(betterRollup({
-            plugins: [
-                rollupBabel({
-                    presets: [
-                        ['@babel/preset-env', {
-                            targets: {
-                                ie: 11,
-                                browsers: 'last 2 versions'
-                            },
-                            useBuiltIns: 'usage',
-                            modules: false,
-                            debug: true
-                        }]
-                    ],
-                    ignore: ['node_modules']
-                })
-            ]
-        }, [{
-            file: pkg.module,
-            format: 'es'
-        }, {
-            file: pkg.browser,
-            format: 'iife'
-        }, {
-            file: pkg.main,
-            format: 'cjs'
-        }]))
-        .pipe(gulp.dest('dist'));
+    return rollupStream({
+        input: config.paths.index,
+        plugins: [
+            rollupBabel(config.babel)
+        ],
+        rollup: rollup,
+        format: 'iife',
+        name: 'jazzer'
+    })
+    .pipe(source('jazzer.js'))
+    .pipe(gulp.dest(config.paths.dist));
 });
 
-gulp.task('default', callback => {
-    runSequence('clean', 'make', callback);
-});
+gulp.task('default', gulp.series('clean', 'make'));
